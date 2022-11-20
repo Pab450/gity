0c7dc6a60ac87b72ca4fb50448cd86c23b00dabd
Pab45O <pablobo@icloud.com>
2022-11-20 02:15:10 +0100

    cleanup
    - better use of the light-server package
    - deletion of the nodemon package which becomes useless
    - some other small cleaning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}', './site/**/*.md'],
-  theme: {
-    extend: {}
-  },
-  plugins: [
-    require('@tailwindcss/typography')
-  ],
-  darkMode: 'class'
+    content: [
+        './src/**/*.{html,js}', 
+        './site/**/*.md'
+    ],
+    theme: {
+      extend: {}
+    },
+    plugins: [
+      require('@tailwindcss/typography')
+    ],
+    darkMode: 'class'
 }
